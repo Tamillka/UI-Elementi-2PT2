5adf43b6e05b1c634dcc123f23ed63921d1f241c
Tamila <Tamila>
2023-04-19 12:22:09 +0300

    Sagatovots pirmas skripta rakstisanai

---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VardaParadisana : MonoBehaviour {
+	public int kadruSk = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Usakta programma! Tu esi " + kadruSk + " kadra.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Kadrs: " + kadruSk);
+		kadruSk++;
+	}
+}
