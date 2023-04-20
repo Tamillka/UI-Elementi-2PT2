68c189a5cc253d3f6663983ad4a6397a3cfa7f46
Tamila <Tamila>
2023-04-20 12:16:49 +0300

    Realizets drop down

---
+++ Assets/Skripti/AttelaParadisana.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttelaParadisana : MonoBehaviour {
+
+	public GameObject beensImg;
+	public GameObject lacisImg;
+    public GameObject masinaImg;
+    public GameObject tanteImg;
+    public GameObject paKreisiPoga;
+    public GameObject paLabiPoga;
+    public GameObject mainigaisAttels;
+    public Sprite[] atteluMasivs;
+
+    public void binaAttelosana(bool vertiba) {
+        beensImg.SetActive(vertiba);
+        paKreisiPoga.GetComponent<Toggle>().interactable = vertiba;
+        paLabiPoga.GetComponent<Toggle>().interactable = vertiba;
+    }
+    public void lacisAttelosana(bool vertiba)
+    {
+        lacisImg.SetActive(vertiba);
+    }
+    public void masinaAttelosana(bool vertiba)
+    {
+        masinaImg.SetActive(vertiba);
+    }
+    public void tanteAttelosana(bool vertiba)
+    {
+        tanteImg.SetActive(vertiba);
+    }
+
+    public void paKreisiBeens(bool vertiba) {
+        beensImg.transform.localScale = new Vector2(1, 1);
+    }
+    public void paLabiBeens(bool vertiba) {
+        beensImg.transform.localScale = new Vector2(-1, 1);
+    }
+    public void Izkritosais(int skaitlis) {
+        if (skaitlis == 0)
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[0];
+        else if (skaitlis == 1)
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[1];
+        else if (skaitlis == 2)
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[2];
+        else
+            Debug.Log("Nav piesaistits attels!");
+    }
+}
--- Assets/Skripti/VardaParadisana.cs
+++ Assets/Skripti/VardaParadisana.cs
@@ -1,19 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VardaParadisana : MonoBehaviour {
-	public int kadruSk = 0;
+	public string teksts;
+	public GameObject ievadesLauks;
+	public GameObject tekstaAttelosana;
+	private string[] fragmenti = { "Helloo ", "Good luck!", "Nahuja pripersja ", "Ljubvi, udachi, terpenija, POKA!" };
+	int indekss;
+	public void UzglabatTekstu() {
+		indekss = Random.Range(0, fragmenti.Length);
+		teksts = ievadesLauks.GetComponent<InputField>().text;
+		tekstaAttelosana.GetComponent<Text>().text=
+		fragmenti[indekss] + teksts.ToUpper() + "!";
+	}
 
 
-	// Use this for initialization
-	void Start () {
-		Debug.Log("Usakta programma! Tu esi " + kadruSk + " kadra.");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Debug.Log("Kadrs: " + kadruSk);
-		kadruSk++;
-	}
 }
